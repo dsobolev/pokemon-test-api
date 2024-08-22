d14579352cca87ad6e90acb6069e42684a4b537c
dsobolev <denis.sobolev1@gmail.com>
2024-08-22 23:18:17 +0100

    Vue/Router setup

---
+++ resources/js/views/MainView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>MainView Here</h1>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\PokemonController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('index');
+});
+
 Route::get('/api/pokemons/{page?}', [PokemonController::class, "all"]);
 Route::get('/api/pokemons/profile/{id}', [PokemonController::class, "single"])
     ->where('id', '[A-Za-z0-9]+');
