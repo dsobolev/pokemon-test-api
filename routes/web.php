203a36687957bd8862c48f235298341d2af9f8c0
dsobolev <denis.sobolev1@gmail.com>
2024-08-23 11:27:43 +0100

    Allow hard refresh on any Vue route

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,7 @@ Route::get('/', function () {
 Route::get('/api/pokemons/{page?}', [PokemonController::class, "all"]);
 Route::get('/api/pokemons/profile/{id}', [PokemonController::class, "single"])
     ->where('id', '[A-Za-z0-9]+');
+
+Route::get('/{vue_capture?}', function () {
+    return view('index');
+})->where('vue_capture', '[\/\w\.-]*');
