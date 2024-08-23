efc42a487c7aa95928c52fc4cae1e3745882a403
dsobolev <denis.sobolev1@gmail.com>
2024-08-23 14:41:34 +0100

    Filter component

---
+++ resources/js/components/Filter.vue
@@ -0,0 +1,47 @@
+<script setup>
+const emit = defineEmits({
+    doFilter: ({ filter }) => {
+        if ( /^(\w|\d)+$/.test(filter) ) {
+            return true
+        } else {
+            console.warn('Please use letters and digits only')
+            return false
+        }
+    },
+})
+const filterValue = defineModel()
+
+function filterEmit() {
+    emit('doFilter', { filter: filterValue.value })
+    console.log('emited', filterValue.value)
+}
+</script>
+
+<template>
+    <div>
+        <input v-model="filterValue" />
+        <button @click="filterEmit">Search</button>
+    </div>
+</template>
+
+<style scoped>
+    input, button {
+        font-size: 24px;
+        border-radius: .5em;
+        padding: .2em 1em;
+        border: 3px solid grey;
+    }
+
+    input {
+        margin-right: .5em;
+    }
+
+    button {
+        background-color: transparent;
+        color: grey;
+    }
+
+    button:hover {
+        background-color: lightgrey;
+    }
+</style>
